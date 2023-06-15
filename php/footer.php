ca3b678deee0b7d98baa166d27dd6bee0e560316
Maxime <maxime.le-besnerais@epitech.eu>
2023-06-15 17:02:11 +0200

    [ALL] 1 file were committed 	Files: 		php/footer.php: Modified
    	Comment:
    		Committed all files

--- php/footer.php
+++ php/footer.php
@@ -1,4 +1,5 @@
-<footer>
+<!--  Include style to make the footer at the bottom of the page -->
+<footer style="position: absolute; bottom: 0; width: 100%; text-align: center;">
     <p>&copy; 2023 Maxime Le Besnerais - All rights reserved</p>
 </footer>
 
