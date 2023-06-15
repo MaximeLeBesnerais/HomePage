e81c094d579e0924ee640ca654bde1d2049f89de
Maxime <maxime.le-besnerais@epitech.eu>
2023-06-15 17:08:53 +0200

    [ALL] 2 files were committed 	Files: 		css/basic.css: Modified 		index.php: Modified
    	Comment:
    		Committed all files

--- index.php
+++ index.php
@@ -3,20 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Website Under Maintenance</title>
-    <link rel="stylesheet" href="css/basic.css"> <!-- assuming your css is in styles.css file -->
+    <link rel="stylesheet" href="css/basic.css">
 </head>
 <body>
-    <header>
+    <header id="banner">
+        <a href="https://www.linkedin.com/in/mlebes/" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png" alt="LinkedIn">
+        </a>
+        <a href="https://github.com/MaximeLeBesnerais" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="GitHub">
+        </a>
     </header>
     <main>
-        <div id="banner">
-            <a href="https://www.linkedin.com/in/mlebes/" target="_blank">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png" alt="LinkedIn">
-            </a>
-            <a href="https://github.com/MaximeLeBesnerais" target="_blank">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="GitHub">
-            </a>
-        </div>
         <div>
             <img src="assets/gear.png" alt="Maintenance Icon">
             <h1>Website is not ready yet</h1>
