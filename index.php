116f5540e01e1acccd04f5dfced95497428fe5f5
Maxime <maxime.le-besnerais@epitech.eu>
2023-06-15 17:05:51 +0200

    [ALL] 3 files were committed 	Files: 		css/basic.css: Modified 		index.php: Modified 		php/header.php: Modified
    	Comment:
    		Committed all files

--- index.php
+++ index.php
@@ -3,71 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <title>Website Under Maintenance</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            background-color: #f3f3f3;
-            color: #333;
-            text-align: center;
-        }
-        img {
-            width: 100px;
-            height: 100px;
-            margin-bottom: 20px;
-        }
-        #banner {
-            position: absolute;
-            top: 0;
-            padding: 10px;
-            width: 100%;
-            background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
-            color: #fff;
-            display: flex;
-            justify-content: flex-end;
-        }
-        #banner a {
-            margin-left: 10px;
-        }
-        #banner img {
-            width: 35px;
-            height: 35px;
-        }
-        @media (max-width: 600px) {
-            #banner {
-                justify-content: center;
-                padding-top: 20px;
-            }
-            img {
-                width: 80px;
-                height: 80px;
-            }
-            #banner img {
-                width: 30px;
-                height: 30px;
-            }
-        }
-    </style>
+    <link rel="stylesheet" href="css/basic.css"> <!-- assuming your css is in styles.css file -->
 </head>
 <body>
-    <div id="banner">
-        <a href="https://www.linkedin.com/in/mlebes/" target="_blank">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png" alt="LinkedIn">
-        </a>
-        <a href="https://github.com/MaximeLeBesnerais" target="_blank">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="GitHub">
-        </a>
-    </div>
-    <div>
-        <img src="assets/gear.png" alt="Maintenance Icon">
-        <h1>Website is not ready yet</h1>
-        <p>I am working on it, please come back later.</p>
-    </div>
+    <header>
+    </header>
+    <main>
+        <div id="banner">
+            <a href="https://www.linkedin.com/in/mlebes/" target="_blank">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png" alt="LinkedIn">
+            </a>
+            <a href="https://github.com/MaximeLeBesnerais" target="_blank">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="GitHub">
+            </a>
+        </div>
+        <div>
+            <img src="assets/gear.png" alt="Maintenance Icon">
+            <h1>Website is not ready yet</h1>
+            <p>I am working on it, please come back later.</p>
+        </div>
+    </main>
+    <footer>
+        <?php include('php/footer.php'); ?>
+    </footer>
 </body>
-<?php include('php/footer.php'); ?>
 </html>
---
+++ php/header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1><?php echo $pageTitle; ?></h1>
+    <nav>
+        <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="cv.php">CV</a></li>
+        </ul>
+    </nav>
+</header>
